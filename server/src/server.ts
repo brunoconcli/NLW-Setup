2867fde9b5007d2dfd04a450f6c8e8024b7e8c7e
Bruno <brunoconcli@gmail.com>
2023-01-19 09:50:41 -0300

    📤 | first commit

---
+++ server/src/server.ts
@@ -0,0 +1,28 @@
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client'
+
+
+const app = Fastify()
+const prisma = new PrismaClient()
+
+// Get, Post, Put, Patch, Delete
+// Promis
+app.register(cors)
+
+app.get('/', async () => {
+    const habits = await prisma.habit.findMany({
+        where: {
+            title: {
+                startsWith: 'Ler'
+            }
+        }
+    })
+    return habits
+})
+
+app.listen ({
+    port: 3333,
+}).then(() => {
+    console.log('HTTP Server is running!') 
+})
---
+++ web/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './styles/global.css';
+
+import { Habit } from './components/Habit'
+
+function App() {
+  return (
+    <div>
+      <Habit completed={3}/>
+      <Habit completed={10}/>
+      <Habit completed={7}/>
+      <Habit completed={4}/>
+      <Habit completed={8}/>
+      
+    </div>
+  )
+}
+
+export default App
