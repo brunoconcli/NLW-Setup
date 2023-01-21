e2ce7a96c6315dbbd5d2733d444ff7520e3a8875
Bruno <brunoconcli@gmail.com>
2023-01-20 21:51:02 -0300

    📑 | replacing `web` files to `typeScript`

---
+++ web/src/components/HabitDay.tsx
@@ -0,0 +1,12 @@
+
+// adding properties to each habit square
+// interface HabitDayProps {
+//     completed: number
+// }
+
+export function HabitDay() {
+    return (
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+        
+    )
+}
